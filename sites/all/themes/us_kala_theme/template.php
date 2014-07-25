e54d9c609a089accde36c325b32120df6e37326e
Scott Rigby <scott@r6by.com>
2014-07-25 23:28:21 +0000

    Better list items

--- sites/all/themes/us_kala_theme/template.php
+++ sites/all/themes/us_kala_theme/template.php
@@ -5,41 +5,11 @@
  */ 
 
 function us_kala_theme_process_page(&$variables) {
-  #kalatheme_process_page(&$variables);
-
-//  $triangle = array(
-//    '#theme'  => 'item_list',
-//    '#items'  => array_fill(0, 21, array('data' => '', 'class' => 'triangle')),
-//    '#attributes' => array('class' => 'triangles'),
-//  );
-//
-//  #$variables['triangle'] = drupal_render($triangle);
-//
-//  $variables['triangle'] = $triangle;
-
-  $variables['triangle'] = '
-  <ul class="triangles">
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-    <li class="triangle"></li>
-  </ul>
-  ';
+  $triangle[] = array(
+    '#theme' => 'item_list',
+    '#items' => array_fill(0, 21, array('data' => '', 'class' => array('triangle'))),
+    '#type' => 'ul',
+    '#attributes' => array('class' => 'triangles'),
+  );
+  $variables['triangle'] = drupal_render($triangle);
 }
