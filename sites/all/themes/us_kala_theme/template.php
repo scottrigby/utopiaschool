048381457bb9bf51e907f19ae6514d2c814ea650
Scott Rigby <scott@r6by.com>
2014-08-16 18:48:45 +0000

    Remove taxonomy term reference field label H3

--- sites/all/themes/us_kala_theme/template.php
+++ sites/all/themes/us_kala_theme/template.php
@@ -13,3 +13,28 @@ function us_kala_theme_process_page(&$variables) {
   );
   $variables['triangle'] = drupal_render($triangle);
 }
+
+/**
+ * Implements theme_field__field_type().
+ */
+function us_kala_theme_field__taxonomy_term_reference($variables) {
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+  }
+  $output .= '</div>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
