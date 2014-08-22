b3f819b311551c10acb6795ca35a8a97e688abc0
Scott Rigby <scott@r6by.com>
2014-08-22 15:43:12 +0000

    working logo example. Needs cleanup

--- sites/all/themes/us_kalatheme/template.php
+++ sites/all/themes/us_kalatheme/template.php
@@ -5,13 +5,37 @@
  */ 
 
 function us_kalatheme_process_page(&$variables) {
-  $triangle[] = array(
-    '#theme' => 'item_list',
-    '#items' => array_fill(0, 21, array('data' => '', 'class' => array('triangle'))),
-    '#type' => 'ul',
-    '#attributes' => array('class' => 'triangles'),
-  );
-  $variables['triangle'] = drupal_render($triangle);
+//  $triangle[] = array(
+//    '#theme' => 'item_list',
+//    '#items' => array_fill(0, 21, array('data' => '', 'class' => array('triangle'))),
+//    '#type' => 'ul',
+//    '#attributes' => array('class' => 'triangles'),
+//  );
+//  $variables['triangle'] = drupal_render($triangle);
+
+  // Get our 65x65 square thumbnail image style.
+  // @todo Add this logo to it's own template.
+  $triangle = '
+  <svg id="logo" width="130" height="112">
+        <pattern id="1" patternUnits="userSpaceOnUse" width="65" height="65">
+            <image xlink:href="/sites/all/themes/us_kalatheme/images/8.jpg" x="0" y="0" width="65" height="65" />
+        </pattern>
+        <pattern id="2" patternUnits="userSpaceOnUse" width="65" height="65">
+            <image xlink:href="/sites/all/themes/us_kalatheme/images/2.jpg" x="0" y="0" width="65" height="65" />
+        </pattern>
+        <pattern id="3" patternUnits="userSpaceOnUse" width="65" height="65" patternTransform="rotate(180)">
+            <image xlink:href="/sites/all/themes/us_kalatheme/images/3.jpg" x="0" y="0" width="65" height="65" />
+        </pattern>
+        <pattern id="4" patternUnits="userSpaceOnUse" width="65" height="86">
+            <image xlink:href="/sites/all/themes/us_kalatheme/images/4.jpg" x="0" y="0" width="65" height="85" />
+        </pattern>
+        <path class="tri one" d="M 32.5,0 L 65,56 L 0,56 z" fill="url(#1)" transform="translate(32.5,0)" stroke="black" stroke-width="0.5" />
+        <path class="tri two" d="M 32.5,0 L 65,56 L 0,56 z" fill="url(#2)" transform="translate(0,56)" stroke="black" stroke-width="0.5" />
+        <path class="tri three" d="M 32.5,0 L 65,56 L 0,56 z" fill="url(#3)" transform="rotate(180,65,65)  translate(32.5,18)" stroke="black" stroke-width="0.5" />
+        <path class="tri four" d="M 32.5,0 L 65,56 L 0,56 z" fill="url(#4)" transform="translate(65,56)" stroke="black" stroke-width="0.5" />
+    </svg>
+  ';
+  $variables['triangle'] = $triangle;
 }
 
 /**
