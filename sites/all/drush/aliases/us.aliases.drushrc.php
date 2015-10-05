56f7ed822c2d633568236d52a9f0ccad3965634a
Scott Rigby <scott@r6by.com>
2015-10-05 17:50:57 -0400

    Drush aliases

---
+++ sites/all/drush/aliases/us.aliases.drushrc.php
@@ -0,0 +1,8 @@
+<?php
+
+$aliases['prod'] = array(
+  'root' => '/home/utopiaschool/public_html',
+  'uri' => 'utopiaschool.org',
+  'remote-host' => 'li283-91.members.linode.com',
+  'remote-user' => 'utopiaschool',
+);
